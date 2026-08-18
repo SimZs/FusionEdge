5e736509c387a6ebf8d8573ffa3856141cd5f84f
SimZs <44397417+SimZs@users.noreply.github.com>
2026-08-18 16:46:17 +0200

    Update AXS and Gen4 setup options

--- gen4-ESP32-35CT_options/myoptions.h
+++ gen4-ESP32-35CT_options/myoptions.h
@@ -6,8 +6,28 @@
 #endif
 
 #define LANGUAGE HU
+#define CLOCK_TTS_LANGUAGE "hu" //Default language TTS e.g. pl,en,de,ru,fr,hu
+// #define IMPERIALUNIT // English weather: Fahrenheit, inHg and mph (requires LANGUAGE EN)
+
+// Optional current-track album art from Last.fm (WEB, SD, DLNA and Bluetooth metadata).
+// #define USE_LASTFM_COVER
+// #define LASTFM_API_KEY "your_lastfm_api_key"
+
+// Animated cassette replaces the clock during the non-blank "While playing" screensaver.
+// #define USE_CASSETTE_SCREENSAVER
+// #define CASSETTE_FRAME_MS 100UL // Reel animation interval; lower values increase display load.
+// #define CASSETTE_PNG_PATH "/images/screensaver/retro_audio_cassette.png"      // Silver/green cassette.
+// #define CASSETTE_PNG_PATH "/images/screensaver/retro_audio_cassette_blue.png" // Blue/pink cassette.
+// #define CASSETTE_PNG_PATH "/images/screensaver/retro_audio_cassette_red.png"  // Red/cream cassette.
+
+// --- DLNA ---
+//#define USE_DLNA
+//#define dlnaHost "192.168.180.122" // your DLNA server IP address
+//#define dlnaIDX  21 //(Music folder index: Synology = 21, MiniDLNA = 1, etc.)
+
+/* -- Névnapok megjelenítése -- Display name days --
+Supported languages: HU, PL, NL, GR, DE (UA Local/namedays/namedays_UA.h is not filled in.) */
 #define NAMEDAYS_FILE HU
-#define CLOCK_TTS_LANGUAGE "hu"
 
 #define USE_BUILTIN_LED false
 
